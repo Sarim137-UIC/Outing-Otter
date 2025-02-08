94ae0cc8d1c1bd1a115d6e87a28971d83f3799c7
Sarim <Sarim137@outlook.com>
2025-02-08 02:57:49 -0600

    Initial Commit

---
+++ Other-Otter/Other_OtterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Other_OtterApp.swift
+//  Other-Otter
+//
+//  Created by Sarim Siddiqui on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Other_OtterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Other-OtterUITests/Other_OtterUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Other_OtterUITestsLaunchTests.swift
+//  Other-OtterUITests
+//
+//  Created by Sarim Siddiqui on 2/8/25.
+//
+
+import XCTest
+
+final class Other_OtterUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
